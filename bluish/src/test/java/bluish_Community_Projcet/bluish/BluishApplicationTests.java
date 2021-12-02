f572fa3b9ebaaf794e520cf592163de1036e0e16
NamHyeop Kim <skaguq@gmail.com>
2021-12-03 03:10:59 +0900

    add file

---
+++ bluish/src/test/java/bluish_Community_Projcet/bluish/BluishApplicationTests.java
@@ -0,0 +1,13 @@
+package bluish_Community_Projcet.bluish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
